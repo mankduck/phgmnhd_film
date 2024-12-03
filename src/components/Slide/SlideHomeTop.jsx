997a34fd0129b3b733a320519b20bc78833de1b7
Phùng Mạnh Đức <phungmanhduc2209@gmail.com>
2024-12-03 17:14:58 +0700

    Commit

--- src/components/Slide/SlideHomeTop.jsx
+++ src/components/Slide/SlideHomeTop.jsx
@@ -5,7 +5,7 @@ const SlideHomeTop = () => {
         <div className="slider-area bg-black">
             <div className="container-fluid p-0">
                 <div className="hero-slider-four dot-style-1 nav-style-1">
-                    <div className="single-hero-slider-wrap single-animation-wrap slider-height-hm4 bg-image-hm4 slider-bg-color-black d-flex align-items-center slider-bg-position-1 bg-black" style={{ backgroundImage: 'url(src/assets/images/slider/slider-hm4-1.jpg)' }}>
+                    <div className="single-hero-slider-wrap single-animation-wrap slider-height-hm4 bg-image-hm4 slider-bg-color-black d-flex align-items-center slider-bg-position-1 bg-black" style={{ backgroundImage: 'url(%PUBLIC_URL%/assets/images/slider/slider-hm4-1.jpg)' }}>
                         <div className="slider-content-hm4 slider-animated">
                             <h1 className="title animated">Out Of Network </h1>
                             <div className="sub-title-time-wrap">
@@ -17,7 +17,7 @@ const SlideHomeTop = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="single-hero-slider-wrap single-animation-wrap slider-height-hm4 bg-image-hm4 slider-bg-color-black d-flex align-items-center slider-bg-position-2 bg-black" style={{ backgroundImage: 'url(src/assets/images/slider/slider-hm4-1.jpg)' }}>
+                    <div className="single-hero-slider-wrap single-animation-wrap slider-height-hm4 bg-image-hm4 slider-bg-color-black d-flex align-items-center slider-bg-position-2 bg-black" style={{ backgroundImage: 'url(%PUBLIC_URL%/assets/images/slider/slider-hm4-1.jpg)' }}>
                         <div className="slider-content-hm4 slider-animated">
                             <h1 className="title animated">Out Of Network </h1>
                             <div className="sub-title-time-wrap">
