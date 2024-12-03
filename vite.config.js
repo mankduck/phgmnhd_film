0dcb1c43006433df0d24bb5ba4d1f74be71fab3b
Phùng Mạnh Đức <phungmanhduc2209@gmail.com>
2024-12-03 16:56:31 +0700

    Commit

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist', // Chỉ định thư mục chứa các tệp build
-  },
-  // Nếu cần, bạn có thể cấu hình lại thư mục công khai (mặc định là 'public')
-  publicDir: 'src',
+  plugins: [react()]
 })
