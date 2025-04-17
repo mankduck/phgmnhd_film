3e703ae5426658b739449e1802570c35df68c369
Phùng Mạnh Đức <phungmanhduc2209@gmail.com>
2025-04-17 14:46:14 +0700

    Add target Blank

--- src/components/Frontend/Footer/Footer.jsx
+++ src/components/Frontend/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
                     <div className="row container">
                         <div className="widget about col-xs-12 col-sm-12 col-md-12 text-center">
                             Liên hệ QC:{" Sợ nên không dám để thông tin!!!"} <br></br><br></br>
-                            Website phim 18+ mới với những bộ phim được cập nhật không ngừng, đa dạng thể loại tại <a href="https://buomto.top/"><span className="text-danger">BUOMTO</span>.top</a>
+                            Website phim 18+ mới với những bộ phim được cập nhật không ngừng, đa dạng thể loại tại <a href="https://buomto.top/" target="_blank" rel="noopener noreferrer"><span className="text-danger">BUOMTO</span>.top</a>
                         </div>
                     </div>
                 </div>
