935aa82db5cb497c9b9581b3f634a710d10cb90d
Phùng Mạnh Đức <phungmanhduc2209@gmail.com>
2025-04-28 09:25:29 +0700

    Commit add bot

--- src/components/Frontend/Footer/Footer.jsx
+++ src/components/Frontend/Footer/Footer.jsx
@@ -9,7 +9,8 @@ const Footer = () => {
                 <div className="container footer-columns">
                     <div className="row container">
                         <div className="widget about col-xs-12 col-sm-12 col-md-12 text-center">
-                            Liên hệ QC:{" Sợ nên không dám để thông tin!!!"} <br></br><br></br>
+                            Liên hệ QC:<a href="https://t.me/PhimCuContactBot" target="_blank">Liên hệ với chúng tôi tại đây!</a>
+                            <br></br><br></br>
                             Website phim 18+ mới với những bộ phim được cập nhật không ngừng, đa dạng thể loại tại <a href="https://buomto.top/" target="_blank" rel="noopener noreferrer"><span className="text-danger">BUOMTO</span>.top</a>
                         </div>
                     </div>
