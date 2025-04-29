5bb7059a9405525f2cefa2d7bfa6439a0d51c9b4
Phùng Mạnh Đức <phungmanhduc2209@gmail.com>
2025-04-29 09:18:15 +0700

    Fix choose server

--- src/components/Frontend/Footer/Footer.jsx
+++ src/components/Frontend/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
                 <div className="container footer-columns">
                     <div className="row container">
                         <div className="widget about col-xs-12 col-sm-12 col-md-12 text-center">
-                            Liên hệ QC:<a href="https://t.me/PhimCuContactBot" target="_blank">Liên hệ với chúng tôi tại đây!</a>
+                            Liên hệ QC:<a href="https://t.me/PhimCuContactBot" target="_blank" rel="noreferrer">Liên hệ với chúng tôi tại đây!</a>
                             <br></br><br></br>
                             Website phim 18+ mới với những bộ phim được cập nhật không ngừng, đa dạng thể loại tại <a href="https://buomto.top/" target="_blank" rel="noopener noreferrer"><span className="text-danger">BUOMTO</span>.top</a>
                         </div>
